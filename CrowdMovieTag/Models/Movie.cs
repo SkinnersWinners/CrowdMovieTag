7f902933bc86fb1d606eee1f64f9ef4e2240310a
Sam Howes <showes06@gmail.com>
2014-04-18 18:20:44 -0400

    Added Data Model Classes Changes: - Added Movie and Tag Data Model Classes for Model Binding with ASP Controls - Added MovieContext as a DatabaseContext for data Access

---
+++ CrowdMovieTag/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrowdMovieTag.Models
+{
+	public class Movie
+	{
+		[ScaffoldColumn(false)]
+		public int MovieID { get; set; }
+
+		[Required, StringLength(100), Display(Name = "Title")]
+		public string Title { get; set; }
+
+		[Required, Range(1000,10000), Display(Name = "Year")]
+		public int Year { get; set; }
+
+		[Required, StringLength(10000), Display(Name = "Movie Description")]
+		public string Description { get; set; }
+
+		[DisplayName("Movie Poster URL"), StringLength(1024)]
+		public string MoviePosterURL { get; set; }
+	}
+}
